234dddb2750c2c31225a5a4b3a62a3f6b6904535
William Xie <wxie@cs.utexas.edu>
2015-12-01 02:04:15 -0600

    Client able to read AR messages

--- bwi_object_search/src/ar_object_transform.cpp
+++ bwi_object_search/src/ar_object_transform.cpp
@@ -5,7 +5,7 @@ void processing (const ar_pose::ARMarkers::ConstPtr& msg){
 
     ar_pose::ARMarker ar_pose_marker;
 
-    for(size_t i=0;i<=msg->markers.size();i++){
+    for(int i=0; i < msg->markers.size();i++){
       ar_pose_marker = msg->markers.at(i);
       std::cout<<"x " << ar_pose_marker.pose.pose.position.x<<std::endl;
       std::cout<<"y " << ar_pose_marker.pose.pose.position.y<<std::endl;
@@ -18,12 +18,12 @@ void processing (const ar_pose::ARMarkers::ConstPtr& msg){
 int main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "locateCamera");
+  ros::init (argc, argv, "ar_object_transform");
   ros::NodeHandle nh;
 
 
   // Create a ROS subscriber
-  ros::Subscriber sub = nh.subscribe ("input", 1, processing);
+  ros::Subscriber sub = nh.subscribe ("ar_pose_marker", 1, processing);
 
   ros::spin ();
 }
